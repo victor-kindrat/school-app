daa4262cc69d020208a5809cd0bb00d424480af2
Victor Kindrat <kinrdatvictor07@gmail.com>
2022-10-15 13:33:23 +0300

    setup

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const port = 8000;
 const express = require('express');
 const open = require('open');
+const fs = require('fs');
 let app = express();
+let bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/messagesended', (req, res) => {
+    res.sendFile(__dirname + '/public/pages/messagesended.html');
+})
+
+app.post('/', (req, res) => {
+    let data = req.body.message;
+    let file = fs.readFileSync('messages.txt') || '';
+    fs.writeFileSync('messages.txt', `${file}\n${data}`);
+    res.redirect(`http://localhost:${port}/messagesended`)/messagesended
 })
 
 app.listen(port, () => {
